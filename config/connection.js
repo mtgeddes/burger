4a73c10afc28ef824b359a2bb52783b0338e285e
Matt <matthewgeddes@yahoo.com>
2018-07-16 22:11:58 -0400

    added more changes

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ let mysql = require("mysql");
 let connection; 
 
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -15,7 +15,7 @@ let connection;
     password: process.env.password,
     database: "burgers_db"
   });
-// }
+}
 
 
 // Make connection.
